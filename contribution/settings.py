307ed2dfbae7476fb98fcb1bca2f7b70b937d742
Franksabu <franksabushimike@gmail.com>
2025-01-10 09:28:59 +0200

    Résolution des conflits de fusion3 sur settinds.py

--- contribution/settings.py
+++ contribution/settings.py
@@ -118,16 +118,6 @@ DATABASES = {
 #     },
 # }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "constructionDB",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    },
-}
 
 # DATABASES = {
 #     'default': {
